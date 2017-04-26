8a385d24193f1b6c1cc9843c13d25819c4b1d22d
LLCampos <luis1212@sapo.pt>
2017-04-26 12:52:42 +0100

    teste

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-listaipod = 'C:\Users\Luis\Google Drive\Backup Ficheiros Programas Musica\iPod.txt'
-listamusica = 'C:\Users\Luis\Google Drive\Backup Ficheiros Programas Musica\Lista Musica.txt'
+listaipod = "C:\Users\Luis\Dropbox\data_campos_music\iPod.txt"
+listamusica = "C:\Users\Luis\Dropbox\data_campos_music\Lista Musica.txt"
